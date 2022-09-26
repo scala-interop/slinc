f3c8eb2f4111cfd776ec06491803a5eab0984011
Mark Hammons <markehammons@gmail.com>
2022-09-26 20:29:43 +0200

    Remove polymorphics

--- polymorphics/src/io/gitlab/mhammons/polymorphics/VarHandleHandler.scala
+++
@@ -1,8 +0,0 @@
-package io.gitlab.mhammons.polymorphics
-
-import java.lang.invoke.VarHandle
-
-object VarHandleHandler {
-   def get[A](vh: VarHandle, a: A): Any = vh.get(a)
-   def set[A, B](vh: VarHandle, a: A, b: B): Unit = vh.set(a, b)
-}
